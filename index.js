0c8b954bdfc68cfd39061f1a122e0eef0c222917
umaruchan1126 <148826595+umaruchan1126@users.noreply.github.com>
2024-03-05 19:19:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on('ready', async () => {
 
   const r = new Discord.RichPresence()
     .setApplicationId('1159457353029140521')
-    .setType('ONLINE')
+    .setType('PLAYING')
     .setURL('https://www.twitch.tv/thirstymonster') //Must be a youtube video link 
     .setState('している')
     .setName('ㅤ')
